5bb5425f6c61127cfe5e5f11f5457cb2c338ebf5
KondoHiroka <93377556+KondoHiroka@users.noreply.github.com>
2021-10-30 18:55:24 +0900

    Create TangibleAsset.java
    new file

---
+++ TangibleAsset.java
@@ -0,0 +1,14 @@
+pblic abstract class TangibleAsset{
+  private String name;
+  private int price;
+  private String color;
+  
+  public TangibleAsset(String name,int price,String color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+  public String getName(){return tnis.name;}
+  pubiic int getPrice(){return tnis.price;}
+  public String getColor(){return tnis.color}
+}
